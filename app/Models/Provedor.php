e17649e88ba3059f996b06201a87fdff26ac6d37
MELISSA MONTAÑO <melissavallecilla0821@gmail.com>
2024-04-24 14:23:02 -0500

    modelo y migracion provedor

---
+++ app/Models/Provedor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Provedor extends Model
+{
+    use HasFactory;
+    protected $table = 'provedores';
+
+    protected $fillable = [
+        'nombre',
+        'direccion',
+        'telefono',
+        'email',
+    ];
+
+
+}
